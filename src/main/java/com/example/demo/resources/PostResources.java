3af7a29d734e9bc8e02768e67c8b4aee31c3a13a
Germano Manzoni <germano12344@gmail.com>
2023-03-25 17:41:54 -0300

    consulta com @query

--- src/main/java/com/example/demo/resources/PostResources.java
+++ src/main/java/com/example/demo/resources/PostResources.java
@@ -1,13 +1,17 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Post;
+import com.example.demo.resources.util.URL;
 import com.example.demo.services.PostService;
 
 @RestController
@@ -23,4 +27,10 @@ public class PostResources {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTtile(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 }
