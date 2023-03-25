401d65881cd037495bf2fb5131f6be1592135d6c
Germano Manzoni <germano12344@gmail.com>
2023-03-25 00:06:53 -0300

    update user com PUT

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
